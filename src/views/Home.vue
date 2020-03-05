313a194b86ab1c9152b2b24197721eb2326a91e2
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-05 11:03:30 -0700

    adds rows, cols for general layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="home bg-light"></div>
+  <div class="home bg-light container-fluid">
+    <div class="row d-flex flex-row-reverse">
+      <div class="col-4">WEATHER</div>
+    </div>
+    <div class="row">
+      <div class="col-12">CLOCK</div>
+    </div>
+    <div class="row">
+      <div class="col-4">IMAGE NAME</div>
+      <div class="col-4">QUOTE</div>
+      <div class="col-4">TODO</div>
+    </div>
+  </div>
 </template>
 
 <script>
