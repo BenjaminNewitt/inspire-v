ed3668f4090daab52395e9619705b104ad2c5464
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-05 11:16:57 -0700

    adds getBackgroundImage action, setResource mutation

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,33 @@ import Axios from "axios";
 
 Vue.use(Vuex);
 
+let baseUrl = "//bcw-sandbox.herokuapp.com/;";
+
+let api = Axios.create({
+  baseURL: baseUrl + "api/",
+  timeout: 8000
+});
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    backgroundImage: {},
+    quote: {},
+    weather: {}
+  },
+  mutations: {
+    setResource(state, payload) {
+      state[payload.name] = payload.data;
+    }
+  },
+  actions: {
+    async getBackgroundImage({ commit, dispatch }) {
+      try {
+        let res = await api.get("images");
+        commit("setResource", { name: "backgroundImage", data: res.data });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
   modules: {}
 });
