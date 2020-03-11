1a192b0ed0fb7298e05e77d754c8e18f72ee7d25
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-11 15:17:35 -0600

    draws clock to page

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="clock">
     <!-- <h1 class="text-center">{{ moment(date).format("LT") }}</h1> -->
+    <digital-clock :twelveHour="true" />
   </div>
 </template>
 
 <script>
 // import moment from "moment";
+import DigitalClock from "vue-digital-clock";
 export default {
   name: "Clock",
-  methods: {
-    // moment: function() {
-    //   return moment();
-    // }
+  components: {
+    DigitalClock
   }
+  // methods: {
+  // moment: function() {
+  //   return moment();
+  // }
+  // }
 };
 </script>
 <style scoped></style>
